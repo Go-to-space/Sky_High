dea465ce86511e00b3837d868d34c45fb7cc60c0
Michelle <j.s.frankfort@gmail.com>
2021-06-01 13:05:51 +0200

    Character switch and collision ignore added

--- Assets/scripts/Player_scripts/Player_movement.cs
+++ Assets/scripts/Player_scripts/Player_movement.cs
@@ -6,18 +6,37 @@ public class Player_movement : MonoBehaviour
 {
     public Rigidbody2D PlayerBody; // the rigidbody of the player
     public float MoveSpeed = 5f; // The movement speed of the player
+    public bool character_switch; // check if movement is swapped
     
     public float JumpForce = 2f; // How strong your jump is
     public Transform feet; // Placement of the feet
     public LayerMask groundLayers;
-    
+    public Collider2D robot;
+    public Collider2D astronaut;
+
     float MoveInput; // varibale used to check which input is given
+
+    private void Start()
+    {
+        Physics2D.IgnoreCollision(robot, astronaut); // The colliders of the robot and astonaut while ignore each other.
+    }
 
     // Update is called once per frame
     void Update()
-    {
-        MoveInput = Input.GetAxisRaw("Horizontal2"); // gives a number of -1 to 1 for input on A and D or left and right on the keyboard
+    { 
+        if (Input.GetButtonDown("switch"))
+        {
+            this.character_switch = !this.character_switch;
+        }
 
+        if (character_switch == true)
+        {
+            MoveInput = Input.GetAxisRaw("Horizontal"); // gives a number of -1 to 1 for input on A and D or left and right on the keyboard
+        } else
+        {
+            MoveInput = Input.GetAxisRaw("Horizontal2"); // gives a number of -1 to 1 for input on A and D or left and right on the keyboard
+        }
+        
         if (Input.GetButtonDown("Jump") && IsGrounded()) // if grounded is true lets you jump
         {
             Jump(); // use the function Jump
--- Assets/scripts/robot_movement.cs
+++ Assets/scripts/robot_movement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class robot_movement : MonoBehaviour
 {
     public float MoveSpeed = 5f;
+    public bool character_switch;
 
     public Rigidbody2D PlayerBody;
 
@@ -12,9 +13,20 @@ public class robot_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("switch"))
+        {
+            this.character_switch = !this.character_switch;
+        }
+
         // uses the unity's input manager to read the inputs
         // convert this inputs into movement over the x and y directions
-        movement.x = Input.GetAxisRaw("Horizontal");
+        if (character_switch == true)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal2");
+        } else
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+        }
         movement.y = Input.GetAxisRaw("Vertical");
     }
     void FixedUpdate()
